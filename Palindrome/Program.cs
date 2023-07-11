f00f41c7077f018eeea0a7cbb00b6b3799f83970
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-11 14:54:12 -0700

    Add staged Main code for entry

---
+++ Palindrome/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace PalindromeFinder
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a word to see if it is a palindrome:");
+            string userWord = Console.ReadLine();
+        }
+    }
+}
